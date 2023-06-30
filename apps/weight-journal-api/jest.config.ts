2ac9be7bfda0cc3d6288cdc443cc0767670f939b
Quincy van Deursen <q.vandeursen@student.avans.nl>
2023-06-30 11:40:08 +0200

    Nest added

---
+++ apps/weight-journal-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'weight-journal-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/weight-journal-api',
+};
