c8ae0ab7ecd187021680c2eb1e42fbc8477543c1
Quincy van Deursen <q.vandeursen@student.avans.nl>
2023-07-01 13:05:17 +0200

    Swagger added

--- apps/weight-journal-api/src/main.ts
+++ apps/weight-journal-api/src/main.ts
@@ -5,12 +5,24 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 import { ApiResponseInterceptor } from './app/shared/api.response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('Weight Journal API')
+    .setDescription('Weight Journal API description')
+    .setVersion('1.0')
+    .addTag('Weight Journal')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //settings
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalInterceptors(new ApiResponseInterceptor());
